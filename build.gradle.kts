a4342912c2f2a66e1c8ce1a0e05c4ce3587cf3a8
Jonas Herzig <me@johni0702.de>
2020-07-13 10:45:55 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("maven-publish")
+}
+
+val lwjgl by configurations.creating
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    lwjgl("org.lwjgl.lwjgl:lwjgl_util:2.9.3") {
+        isTransitive = false // only want col, dim, point, vec, mat, quat, etc.
+    }
+}
+
+val relocatedJar = file("$buildDir/relocated.jar")
+val relocate by tasks.creating(ShadowJar::class) {
+    destinationDirectory.set(buildDir)
+    archiveBaseName.set(name)
+    configurations = listOf(lwjgl)
+    exclude("META-INF/**/*")
+
+    relocate("org.lwjgl.util", "de.johni0702.minecraft.gui.utils.lwjgl")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.ReplayMod"
+            artifactId = "lwjgl-utils"
+            version = "1.0"
+
+            artifact(relocate.archiveFile) {
+                builtBy(relocate)
+            }
+        }
+    }
+}
